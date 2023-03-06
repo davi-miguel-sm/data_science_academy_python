8e39d2362de0396afd86b21479ad6e50edc38a22
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-06 19:16:29 -0300

    Condicionais Aninhados

--- Aula06 - Condicionais/condicionais.py
+++ Aula06 - Condicionais/condicionais.py
@@ -17,3 +17,16 @@ elif num ==10:
     print("Aviões do Forró")
 else:
     print("Péricles")
+
+#Condicionais aninhados
+banda = "boa"
+
+if num >10:
+    print("Barões da Pisadinha")
+elif num ==10:
+    if banda == "boa":
+        print("Aviões do Forró")
+    elif banda == "ruim":
+        print("Luisa Sonza")
+else:
+    print("Péricles")
