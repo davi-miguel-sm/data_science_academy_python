c9ab2f07e86b3668abfa552d8aacfd84065f1554
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-06 19:12:57 -0300

    If, Elif e Else

---
+++ Aula06 - Condicionais/condicionais.py
@@ -0,0 +1,19 @@
+# if
+if 10>2:
+    print("Barões da Pisadinha")
+else:
+    print("Aviões do Forró")
+
+num = 9
+
+if num >10:
+    print("Barões da Pisadinha")
+else:
+    print("Aviões do Forró")
+
+if num >10:
+    print("Barões da Pisadinha")
+elif num ==10:
+    print("Aviões do Forró")
+else:
+    print("Péricles")
