96f187ebac4ef4833f60ba8286b69b185bf43866
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-07 19:30:24 -0300

    Operadores or

--- Aula07 - Operadores Lógicos/operadoresLogicos.py
+++ Aula07 - Operadores Lógicos/operadoresLogicos.py
@@ -1,5 +1,12 @@
 #and - True se ambas forem true {É curto circuito, primero valor false interrompe a verificação}
-num = 11
+num = 9
 if num > 10 and num <15:
     print("Docin de Leite")
+else:
+    print("Jiló assado")
 
+#or - True se pelo menos uma for true {É curto circuito, primeiro valor true interrompe a verificação}
+if num > 10 or num <15:
+    print("Docin de maracujá")
+else:
+    print("Pimenta malagueta")
