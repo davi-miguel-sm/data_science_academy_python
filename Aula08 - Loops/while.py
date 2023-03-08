ec4167af861837524f9ad8b04933cd8bc6d2ad9b
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-08 10:51:12 -0300

    Loop While com Break e Continue

--- Aula08 - Loops/while.py
+++ Aula08 - Loops/while.py
@@ -1,8 +1,16 @@
 # Loop While
 
-valor = 0
+valor = 100
 while valor < 10:
     valor += 1
 else:
     print("Saindo do Loop...")
 print(valor)
+
+while valor >= 10:
+    valor -= 1
+    if valor == 11:
+        break
+    if valor == 44:
+        continue
+    print(valor)
