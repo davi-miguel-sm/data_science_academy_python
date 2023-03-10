a4e52a64f4cce4505d1fdb6b47d7e22fe7c50016
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-10 18:21:42 -0300

    Exemplo Modulo Os com WITH

--- Aula11 - Modulo OS/moduloOs.py
+++ Aula11 - Modulo OS/moduloOs.py
@@ -14,3 +14,11 @@ arquivo = open('Aula11 - Modulo OS/cientista.txt','r')
 conteudo = arquivo.read()
 arquivo.close()
 print(conteudo)
+
+Brasil = "O Brasil, um vasto país sul-americano, estende-se da Bacia Amazônica, no norte,\naté os vinhedos e as gigantescas Cataratas do Iguaçu, no sul. O Rio de Janeiro, simbolizado pela sua estátua\nde 38 metros de altura do Cristo Redentor, situada no topo do Corcovado, é famoso pelas movimentadas praias de Copacabana\ne Ipanema, bem como pelo imenso e animado Carnaval, com desfiles de carros alegóricos, fantasias extravagantes e samba. "
+with open('Aula11 - Modulo OS/Brasil.txt','w',encoding="utf-8") as arquivo2:
+    arquivo2.write(Brasil[::-1])
+
+with open('Aula11 - Modulo OS/Brasil.txt','r',encoding='utf-8') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo[::-1])
