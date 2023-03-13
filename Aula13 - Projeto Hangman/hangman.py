624045f056b015831430d170b4627905f05a6f13
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-13 00:02:51 -0300

    Jogo Finalizado

--- Aula13 - Projeto Hangman/hangman.py
+++ Aula13 - Projeto Hangman/hangman.py
@@ -20,3 +20,39 @@ def texto_inicial(traços, chances, letras_erradas):
 def calcula_chances(palavra):
     chances = len(set(palavra))
     return chances
+
+def game():
+    limpa_tela()
+
+    print("\nBem Vindo ao Jogo da Forca\n")
+    print("\nQue os jogos comecem!\n")
+    palavras_forca = conteudo
+    palavra = random.choice(palavras_forca)
+
+    traços_letras = ['_' for letra in palavra]
+    chances = calcula_chances(palavra)
+    letras_erradas = []
+
+    while chances > 0:
+        texto_inicial(traços_letras, chances, letras_erradas)    
+
+        tentativa = input("\nTente a sua sorte:\n").lower()
+
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    traços_letras[index] = letra
+                index +=1
+        else:
+            chances -=1
+            letras_erradas.append(tentativa)
+
+        if "".join(traços_letras) == palavra:
+            texto_inicial(traços_letras, chances, letras_erradas)
+            print("VOCÊ SAIU VIVO DESSA VEZ!")
+            break
+    else:
+        texto_inicial(traços_letras, chances, letras_erradas)
+        print("VOCÊ FOI ELIMINADO!!!")
+game()
