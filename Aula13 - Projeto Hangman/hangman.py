65eff241e37aedef9ed231aca871ed05d7aad420
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-13 00:02:28 -0300

    Funções de exibir o texto e calcular as chances

--- Aula13 - Projeto Hangman/hangman.py
+++ Aula13 - Projeto Hangman/hangman.py
@@ -2,3 +2,21 @@ conteudo = []
 with open('Aula13 - Projeto Hangman\palavras.txt', 'r',encoding='utf-8') as arquivo:
     conteudo = arquivo.read().split()
 print(conteudo)
+
+import random
+from os import system,name
+
+def limpa_tela():
+    if name =='nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+def texto_inicial(traços, chances, letras_erradas):
+    print("".join(traços))
+    print(f"\nChances Restantes: {chances}")
+    print(f"Letras Erradas: ", "".join(letras_erradas))
+
+def calcula_chances(palavra):
+    chances = len(set(palavra))
+    return chances
