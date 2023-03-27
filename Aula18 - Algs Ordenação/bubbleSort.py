23b0752bb6cd7f5da8ac81846ad20b3335250502
Davi Miguel <davi.miguel2020@gmail.com>
2023-03-27 17:38:26 -0300

    Iniciando insertion sort

--- Aula18 - Algs Ordenação/bubbleSort.py
+++ Aula18 - Algs Ordenação/bubbleSort.py
@@ -2,8 +2,8 @@ array = [30, 50, 10, 35, 70, 45, 80, 100, 22]
 tam_array = len(array)
 
 for i in range(tam_array):
-    for j in range(tam_array - 1):
-        if array[j] > array[j+1]:
-            array[j+1], array[j] = array[j], array[j+1]
+    for j in range(tam_array - 1): 
+        if array[j] > array[j+1]: # Compara se o atual é maior que o proximo
+            array[j+1], array[j] = array[j], array[j+1] # Se maior troca as posições
 
 print(array)
